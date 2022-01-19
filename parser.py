5af8c4cdc50ac15e8fd62243aadc771ee1448e93
Burak Tokak <iletisim@buraktokak.com>
2022-01-19 13:54:55 +0300

    add parser script

--- parser.py
+++ parser.py
@@ -7,14 +7,15 @@ from pathlib import Path
 if __name__ =="__main__":
 
 	# Load the language model
-	nlp = spacy.load("en_core_web_sm")
+	nlp = spacy.load("./training/model-best")
 
-	sentence = 'Deemed universities charge huge fees'
+	sentence = 'Gidersem belki olabilir'
 
 
 
 
 	doc = nlp(sentence)
+	print(doc);
 
 	print ("{:<15} | {:<8} | {:<15} | {:<20}".format('Token','Relation','Head', 'Children'))
 	print ("-" * 70)
@@ -30,7 +31,7 @@ if __name__ =="__main__":
 	# for jupiter, uncomment the next line
 	# displacy.render(doc, style='dep', jupyter=True, options={'distance': 120})
 
-	svg = displacy.render(doc, style='dep', options={'distance': 120})
+	# svg = displacy.render(doc, style='dep', options={'distance': 120})
 
-	output_path = Path("./dependency_plot.svg")
-	output_path.open("w", encoding="utf-8").write(svg)
+	# output_path = Path("./dependency_plot.svg")
+	# output_path.open("w", encoding="utf-8").write(svg)
